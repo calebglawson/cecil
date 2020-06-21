d5a3e8a201480a6808f644fb60792fad8eb4e38f
Caleb Lawson <caleb.g.lawson@gmail.com>
2020-06-21 16:40:59 -0400

    initial api

---
+++ main.py
@@ -0,0 +1,43 @@
+'''
+Main file of the FastAPI application, routes live here.
+'''
+
+from typing import List
+from fastapi import FastAPI
+import control
+from models import User
+
+
+APP = FastAPI()
+
+
+@APP.get("/users/", response_model=List[User])
+async def get_users(page: int = 1, page_size: int = 20):
+    '''
+    Get a list of users and top level info in the user directory.
+    '''
+    return control.get_users(page=page, page_size=page_size)
+
+
+@APP.get("/users/{user_id}", response_model=User)
+async def get_user(user_id: int):
+    '''
+    Get a user's top level info.
+    '''
+    return control.get_user(user_id)
+
+
+@APP.post("/users/{user_id}", response_model=User)
+async def add_user(user_id: int):
+    '''
+    Add a user to the directory
+    '''
+    return control.get_user(user_id)
+
+
+@APP.get("/watchlists/", response_model=List[str])
+async def get_watchlists():
+    '''
+    Get a list of watchlists in the watchlist directory.
+    '''
+    return control.get_watchlists()
